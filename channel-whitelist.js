f20ffc142883c4cd090119e8316be7f2f947c6e3
Max Wofford <max@maxwofford.com>
2021-12-18 02:06:11 -0500

    Add channel whitelist

---
+++ channel-whitelist.js
@@ -0,0 +1,10 @@
+const channelWhitelist = id => ({
+  'C0159TSJVH8': 'what-is-my-channel-id',
+  'C0266FRGV': 'lounge',
+  'C0P5NE354': 'bot-spam',
+  'C01EZ2MNPTQ': 'katabatic-wind',
+  'C0EA9S0A0': 'code',
+  'C0M8PUPU6': 'ship',
+  'C01504DCLVD': 'scrapbook',
+  'CLU651WHY': 'orpheus-internals'
+}[id])
